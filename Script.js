cd5a660c6d946f72145526b14bec37899d6d1184
julia1205jc-hash <julia1205jc@gmail.com>
2025-09-12 11:26:08 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,7 +3,12 @@ const mensagens = [
   "Nunca se esqueça: sua vida importa 💛",
   "Você não está sozinho, estamos juntos 💝",
   "Acredite: dias melhores virão 🌹",
-  "Sempre existe esperança 🍀"
+  "Sempre existe esperança 🍀";
+  "Não desista 🌷";
+  "Você é luz 🌟";
+  "Não sofra em silêncio. Falar faz bem e pode aliviar o peso 🌸";
+  "Estamos aqui para te ouvir e te apoiar 💐";
+  "Enquanto houver vida, há esperança ⭐"
 ];
 
 document.getElementById("btnMensagem").addEventListener("click", () => {
